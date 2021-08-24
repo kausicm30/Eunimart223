1b0608753627fd6f047bb024c0d9ad29d5b7138e
kausicm30 <jaikausic@gmail.com>
2021-08-24 19:02:15 +0530

    second commit

---
+++ July/Docker -works/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var axios = require('axios');
+var app = express();
+
+app.get('/', function(req, res)
+{
+    res.send("hello How are you?");
+});
+
+app.listen(3030,function(){
+    console.log('server listening on port 3030');
+});
